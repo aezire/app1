b00ab24c220c76e5792e0f25747b59ba4ab8cd1c
shubh <shubh@LAPTOP-QKESPE8M>
2025-05-10 18:16:26 +0530

    removed unused import and comments

--- src/main/java/com/example/demo/util/JwtUtil.java
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -1,29 +1,3 @@
-//package com.example.demo.util;
-//
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Date;
-//
-//@Component
-//public class JwtUtil {
-//	@Value("${jwt.secret}")
-//    private String secretKey; 
-//    private static final long EXPIRATION_TIME = 86400000; // 24 hours
-//
-//    public String generateToken(String username) {
-//        return Jwts.builder()
-//                .setSubject(username)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-//                .signWith(SignatureAlgorithm.HS256, secretKey)
-//                .compact();
-//    }
-//}
-
 
 package com.example.demo.util;
 
